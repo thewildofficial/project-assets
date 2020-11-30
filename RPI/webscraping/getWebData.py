d98c857e5a43a631a2f8bfeebc14ba4c358089d0
thewildofficial <52071834+thewildofficial@users.noreply.github.com>
2020-11-30 14:04:48 +0530

    webscrapper implimentation

--- RPI/webscraping/getWebData.py
+++ RPI/webscraping/getWebData.py
@@ -1,10 +1,10 @@
 #I imported as bs cause funny
 import json
 import os
-
 from bs4 import BeautifulSoup as BS
 from urllib.request import urlopen
-def retriveData(webpageIP,outputJsonFileName,htmlTag):
+import ast
+def retriveData(webpageIP,outputJsonFileName,htmlTags):
     try:
         html = urlopen(webpageIP)
     except Exception:
@@ -12,16 +12,20 @@ def retriveData(webpageIP,outputJsonFileName,htmlTag):
         file = os.path.join(THIS_FOLDER, webpageIP) #here webpageIP is actually a local webpage file
         with open(file, "r") as f:
             html = f.read()
-
+    texts = {}
     soup = BS(html,features="html.parser")
-    tds = soup.find_all(htmlTag)
-    texts = []
-    for td in tds:
-        inner_text = td.text
-        texts.append(inner_text)
+    for htmlTag in ast.literal_eval(htmlTags):
+        tds = soup.find_all(htmlTag)
+        for td in tds:
+            if htmlTag != "img":
+                texts[td.get("class")[0]] = td.text
+            else:
+                texts["imageLink"] = td.get("src")
+
 
     with open(f'{outputJsonFileName}.json', 'w') as f:
         json.dump(texts, f)
+
     print("done!")
 
 
@@ -31,3 +35,8 @@ def retriveData(webpageIP,outputJsonFileName,htmlTag):
 example case:
     retriveData("testSite.html","test","p")
 """
+if __name__ == "__main__":
+        webpageIP = input("enter the website link or name:")
+        outputJsonFileName = input("what should the json file be called?:")
+        htmlTags = input("enter a PYTHON LIST of html tags to be scrapped:")
+        retriveData(webpageIP,outputJsonFileName,htmlTags)
